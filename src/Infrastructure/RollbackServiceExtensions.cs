443261f3ae3eafbec49e0c6bd7edbba59d5d74a9
Vladyslav Zaiets <rutova2@gmail.com>
2022-05-09 17:34:13 +0300

    Readability pass over RollbackRequest

---
+++ src/Infrastructure/RollbackServiceExtensions.cs
@@ -0,0 +1,27 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using DotNetDeployNotify.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNetDeployNotify.Infrastructure;
+
+/// <summary>
+/// Extension methods for registering rollback services in the dependency injection container
+/// </summary>
+public static class RollbackServiceExtensions
+{
+    /// <summary>
+    /// Adds deployment rollback services to the dependency injection container.
+    /// Call this alongside <see cref="DependencyInjection.AddNotificationServices"/> during setup.
+    /// </summary>
+    /// <param name="services">The service collection to register services into</param>
+    /// <returns>The same service collection for fluent chaining</returns>
+    public static IServiceCollection AddRollbackServices(this IServiceCollection services)
+    {
+        services.AddScoped<IRollbackService, RollbackService>();
+        return services;
+    }
+}
