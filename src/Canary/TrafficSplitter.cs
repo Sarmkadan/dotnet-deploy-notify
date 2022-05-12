2cb247e322183c883d8b037d4c4a23e40cf5f5a9
Vladyslav Zaiets <rutova2@gmail.com>
2022-05-12 07:25:56 +0300

    refactor: dedupe some logic in CanaryDeploymentEngine

---
+++ src/Canary/TrafficSplitter.cs
@@ -0,0 +1,258 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using DotNetDeployNotify.Configuration;
+using DotNetDeployNotify.Core.Models;
+using Microsoft.Extensions.Options;
+
+namespace DotNetDeployNotify.Canary;
+
+/// <summary>
+/// Produces per-request routing decisions and generates strategy-specific rollout plans
+/// based on the current <see cref="CanaryOptions"/> configuration.
+/// </summary>
+public sealed class TrafficSplitter : ITrafficSplitter
+{
+    private readonly CanaryOptions _options;
+    private readonly ILogger<TrafficSplitter> _logger;
+
+    /// <summary>Initialises the splitter with options and a logger</summary>
+    public TrafficSplitter(IOptions<CanaryOptions> options, ILogger<TrafficSplitter> logger)
+    {
+        _options = options.Value;
+        _logger = logger;
+    }
+
+    /// <inheritdoc />
+    public TrafficSplit ComputeNextSplit(CanaryDeployment deployment)
+    {
+        var activeStep = deployment.RolloutPlan
+            .FirstOrDefault(s => s.Status == RolloutStepStatus.InProgress);
+
+        if (activeStep is null)
+        {
+            _logger.LogDebug(
+                "No active rollout step for {Project} — returning current split {Split}",
+                deployment.ProjectName,
+                deployment.CurrentSplit);
+
+            return deployment.CurrentSplit;
+        }
+
+        var split = TrafficSplit.FromCanaryPercent(activeStep.CanaryPercent);
+
+        _logger.LogDebug(
+            "Computed split for {Project} step {Step}/{Total}: {Split}",
+            deployment.ProjectName,
+            activeStep.StepNumber,
+            deployment.RolloutPlan.Count,
+            split);
+
+        return split;
+    }
+
+    /// <inheritdoc />
+    public bool ShouldRouteToCanary(TrafficSplit split)
+    {
+        if (split.CanaryPercent <= 0) return false;
+        if (split.CanaryPercent >= 100) return true;
+
+        return Random.Shared.NextDouble() * 100 < split.CanaryPercent;
+    }
+
+    /// <inheritdoc />
+    public List<CanaryRolloutStep> GenerateRolloutPlan(CanaryStrategy strategy)
+    {
+        var plan = strategy switch
+        {
+            CanaryStrategy.Linear      => GenerateLinearPlan(),
+            CanaryStrategy.Exponential => GenerateExponentialPlan(),
+            CanaryStrategy.BlueGreen   => GenerateBlueGreenPlan(),
+            CanaryStrategy.Shadow      => GenerateShadowPlan(),
+            _                          => GenerateLinearPlan()
+        };
+
+        _logger.LogDebug(
+            "Generated {Strategy} rollout plan: {Steps} step(s), soak {Soak}",
+            strategy,
+            plan.Count,
+            _options.StepSoakDuration);
+
+        return plan;
+    }
+
+    // -------------------------------------------------------------------------
+    // Strategy plan generators
+    // -------------------------------------------------------------------------
+
+    private List<CanaryRolloutStep> GenerateLinearPlan()
+    {
+        var stepCount = Math.Clamp(_options.LinearStepCount, 2, 20);
+        var stepPercent = 100.0 / stepCount;
+
+        return Enumerable.Range(1, stepCount)
+            .Select(i => new CanaryRolloutStep
+            {
+                StepNumber    = i,
+                CanaryPercent = Math.Min(100.0, Math.Round(stepPercent * i, 1)),
+                SoakDuration  = _options.StepSoakDuration
+            })
+            .ToList();
+    }
+
+    private List<CanaryRolloutStep> GenerateExponentialPlan()
+    {
+        double[] percentages = [1, 2, 5, 10, 25, 50, 100];
+
+        return percentages
+            .Select((p, i) => new CanaryRolloutStep
+            {
+                StepNumber    = i + 1,
+                CanaryPercent = p,
+                // Soak duration increases linearly with step index to allow more observation at higher traffic
+                SoakDuration  = _options.StepSoakDuration * (i + 1)
+            })
+            .ToList();
+    }
+
+    private List<CanaryRolloutStep> GenerateBlueGreenPlan() =>
+    [
+        new() { StepNumber = 1, CanaryPercent = 50,  SoakDuration = _options.StepSoakDuration },
+        new() { StepNumber = 2, CanaryPercent = 100, SoakDuration = _options.StepSoakDuration }
+    ];
+
+    private List<CanaryRolloutStep> GenerateShadowPlan() =>
+    [
+        // Step 1: mirror-only — canary handles 0% production traffic while observing request shapes
+        new() { StepNumber = 1, CanaryPercent = 0,   SoakDuration = _options.StepSoakDuration },
+        new() { StepNumber = 2, CanaryPercent = 5,   SoakDuration = _options.StepSoakDuration },
+        new() { StepNumber = 3, CanaryPercent = 25,  SoakDuration = _options.StepSoakDuration * 2 },
+        new() { StepNumber = 4, CanaryPercent = 100, SoakDuration = _options.StepSoakDuration }
+    ];
+}
+
+/// <summary>
+/// Evaluates canary health by comparing live metrics against configured thresholds
+/// and the stable version baseline.
+/// </summary>
+public sealed class CanaryHealthEvaluator : ICanaryHealthEvaluator
+{
+    private readonly CanaryOptions _options;
+    private readonly ILogger<CanaryHealthEvaluator> _logger;
+
+    /// <summary>Initialises the evaluator with options and a logger</summary>
+    public CanaryHealthEvaluator(IOptions<CanaryOptions> options, ILogger<CanaryHealthEvaluator> logger)
+    {
+        _options = options.Value;
+        _logger = logger;
+    }
+
+    /// <inheritdoc />
+    public async Task<CanaryEvaluationResult> EvaluateAsync(
+        CanaryDeployment deployment,
+        CancellationToken cancellationToken = default)
+    {
+        var stableTask = CollectMetricsAsync(deployment.StableVersion, deployment.TargetEnvironment, cancellationToken);
+        var canaryTask = CollectMetricsAsync(deployment.CanaryVersion, deployment.TargetEnvironment, cancellationToken);
+
+        await Task.WhenAll(stableTask, canaryTask);
+
+        var stableMetrics = stableTask.Result;
+        var canaryMetrics = canaryTask.Result;
+
+        var violations = BuildViolationList(stableMetrics, canaryMetrics);
+        var isHealthy = violations.Count == 0;
+
+        var reason = isHealthy
+            ? "All metrics within acceptable thresholds"
+            : string.Join("; ", violations);
+
+        _logger.LogInformation(
+            "Health evaluation for {Project} v{Canary} on step {Step}: {Verdict} — {Reason}",
+            deployment.ProjectName,
+            deployment.CanaryVersion,
+            deployment.ActiveStep?.StepNumber ?? 0,
+            isHealthy ? "HEALTHY" : "UNHEALTHY",
+            reason);
+
+        return new CanaryEvaluationResult
+        {
+            IsHealthy        = isHealthy,
+            Reason           = reason,
+            StableMetrics    = stableMetrics,
+            CanaryMetrics    = canaryMetrics,
+            Violations       = violations,
+            ShouldAutoRollback = !isHealthy && _options.AutoRollbackOnFailure
+        };
+    }
+
+    /// <inheritdoc />
+    public Task<CanaryMetrics> CollectMetricsAsync(
+        string version,
+        Environment environment,
+        CancellationToken cancellationToken = default)
+    {
+        // Pluggable integration point: replace with Prometheus, Datadog, CloudWatch,
+        // Application Insights, or any other observability back-end.
+        // Returns a zero-baseline snapshot appropriate for integration testing.
+        return Task.FromResult(new CanaryMetrics
+        {
+            ErrorRatePercent = 0,
+            P95LatencyMs     = 0,
+            P99LatencyMs     = 0,
+            RequestCount     = 0,
+            ErrorCount       = 0,
+            LastUpdatedAt    = DateTime.UtcNow
+        });
+    }
+
+    // -------------------------------------------------------------------------
+    // Private helpers
+    // -------------------------------------------------------------------------
+
+    private List<string> BuildViolationList(CanaryMetrics stable, CanaryMetrics canary)
+    {
+        var t = _options.Thresholds;
+        var violations = new List<string>();
+
+        if (canary.ErrorRatePercent > t.MaxErrorRatePercent)
+            violations.Add(
+                $"Error rate {canary.ErrorRatePercent:F2}% exceeds absolute threshold of {t.MaxErrorRatePercent}%");
+
+        if (canary.P95LatencyMs > t.MaxP95LatencyMs)
+            violations.Add(
+                $"P95 latency {canary.P95LatencyMs:F0}ms exceeds threshold of {t.MaxP95LatencyMs:F0}ms");
+
+        if (canary.P99LatencyMs > t.MaxP99LatencyMs)
+            violations.Add(
+                $"P99 latency {canary.P99LatencyMs:F0}ms exceeds threshold of {t.MaxP99LatencyMs:F0}ms");
+
+        // Baseline-relative checks — only meaningful when stable has observable traffic
+        if (stable.RequestCount > 0)
+        {
+            if (stable.ErrorRatePercent > 0)
+            {
+                var errorMultiplier = canary.ErrorRatePercent / stable.ErrorRatePercent;
+                if (errorMultiplier > t.ErrorRateMultiplier)
+                    violations.Add(
+                        $"Canary error rate is {errorMultiplier:F1}x the stable baseline " +
+                        $"({canary.ErrorRatePercent:F2}% vs {stable.ErrorRatePercent:F2}%); " +
+                        $"threshold: {t.ErrorRateMultiplier:F1}x");
+            }
+
+            if (stable.P95LatencyMs > 0)
+            {
+                var latencyDelta = (canary.P95LatencyMs - stable.P95LatencyMs) / stable.P95LatencyMs * 100;
+                if (latencyDelta > t.LatencyDegradationPercent)
+                    violations.Add(
+                        $"P95 latency degraded {latencyDelta:F1}% vs stable baseline " +
+                        $"({canary.P95LatencyMs:F0}ms vs {stable.P95LatencyMs:F0}ms); " +
+                        $"threshold: {t.LatencyDegradationPercent:F1}%");
+            }
+        }
+
+        return violations;
+    }
+}
---
+++ src/Core/Models/CanaryDeployment.cs
@@ -0,0 +1,324 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace DotNetDeployNotify.Core.Models;
+
+/// <summary>
+/// Lifecycle status of a canary deployment operation
+/// </summary>
+public enum CanaryStatus
+{
+    /// <summary>Deployment has been created but the first step has not yet started</summary>
+    Pending = 0,
+
+    /// <summary>Canary is actively receiving a split percentage of production traffic</summary>
+    Active = 1,
+
+    /// <summary>Rollout temporarily suspended, awaiting manual approval to continue</summary>
+    Paused = 2,
+
+    /// <summary>Automatic rollback triggered due to health threshold violations</summary>
+    RollingBack = 3,
+
+    /// <summary>Canary successfully promoted to receive 100% of production traffic</summary>
+    Promoted = 4,
+
+    /// <summary>Deployment was manually aborted or failed to complete rollback</summary>
+    Aborted = 5
+}
+
+/// <summary>
+/// Traffic-splitting algorithm used to progress the canary rollout
+/// </summary>
+public enum CanaryStrategy
+{
+    /// <summary>Equal percentage increments at each step (e.g. 20% → 40% → 60% → 80% → 100%)</summary>
+    Linear = 0,
+
+    /// <summary>Exponential progression with conservative early steps (1% → 2% → 5% → 10% → 25% → 50% → 100%)</summary>
+    Exponential = 1,
+
+    /// <summary>Immediate 50/50 split followed by full promotion — suitable for low-risk changes</summary>
+    BlueGreen = 2,
+
+    /// <summary>Mirrored traffic with no initial canary production exposure, then gradual ramp-up</summary>
+    Shadow = 3
+}
+
+/// <summary>
+/// Progress state of a single step within the rollout plan
+/// </summary>
+public enum RolloutStepStatus
+{
+    /// <summary>Step is scheduled but has not yet started soaking</summary>
+    Pending = 0,
+
+    /// <summary>Step is active; the canary is soaking at this traffic level</summary>
+    InProgress = 1,
+
+    /// <summary>Step completed successfully; soak duration elapsed with healthy metrics</summary>
+    Completed = 2,
+
+    /// <summary>Step was bypassed due to a promote or abort action</summary>
+    Skipped = 3,
+
+    /// <summary>Step failed health checks and triggered an automatic rollback</summary>
+    Failed = 4
+}
+
+/// <summary>
+/// Represents a canary deployment with traffic-splitting configuration and full lifecycle tracking
+/// </summary>
+public class CanaryDeployment
+{
+    /// <summary>Unique identifier for this canary deployment</summary>
+    public string Id { get; init; } = Guid.NewGuid().ToString();
+
+    /// <summary>Project or application being deployed</summary>
+    public required string ProjectName { get; init; }
+
+    /// <summary>Currently stable version already serving production traffic</summary>
+    public required string StableVersion { get; init; }
+
+    /// <summary>Candidate version receiving the split canary traffic</summary>
+    public required string CanaryVersion { get; init; }
+
+    /// <summary>Target environment where the rollout is executing</summary>
+    public required Environment TargetEnvironment { get; init; }
+
+    /// <summary>Current lifecycle status of this deployment</summary>
+    public CanaryStatus Status { get; set; } = CanaryStatus.Pending;
+
+    /// <summary>Traffic-splitting algorithm applied to this rollout</summary>
+    public CanaryStrategy Strategy { get; init; } = CanaryStrategy.Linear;
+
+    /// <summary>Current percentage distribution of traffic between stable and canary versions</summary>
+    public TrafficSplit CurrentSplit { get; set; } = TrafficSplit.Initial;
+
+    /// <summary>Ordered sequence of traffic-shift steps generated from the chosen strategy</summary>
+    public List<CanaryRolloutStep> RolloutPlan { get; init; } = [];
+
+    /// <summary>Latest health metrics snapshot for the stable version</summary>
+    public CanaryMetrics StableMetrics { get; set; } = new();
+
+    /// <summary>Latest health metrics snapshot for the canary version</summary>
+    public CanaryMetrics CanaryMetrics { get; set; } = new();
+
+    /// <summary>Channels that receive lifecycle transition notifications</summary>
+    public List<NotificationChannel> NotificationChannels { get; init; } = [];
+
+    /// <summary>Priority assigned to all lifecycle notifications for this deployment</summary>
+    public NotificationPriority Priority { get; init; } = NotificationPriority.High;
+
+    /// <summary>Identity of the person or system that initiated the deployment</summary>
+    public string InitiatedBy { get; init; } = string.Empty;
+
+    /// <summary>Git branch from which the canary version was built</summary>
+    public string BranchName { get; init; } = string.Empty;
+
+    /// <summary>Git commit hash of the canary build</summary>
+    public string CommitHash { get; init; } = string.Empty;
+
+    /// <summary>Link to the CI/CD job that produced the canary artifact</summary>
+    public string BuildUrl { get; init; } = string.Empty;
+
+    /// <summary>Human-readable reason provided when aborting the deployment</summary>
+    public string? AbortReason { get; set; }
+
+    /// <summary>UTC timestamp when this deployment was created</summary>
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+
+    /// <summary>UTC timestamp when the canary was successfully promoted to full traffic</summary>
+    public DateTime? PromotedAt { get; set; }
+
+    /// <summary>UTC timestamp when the deployment was aborted or failed</summary>
+    public DateTime? AbortedAt { get; set; }
+
+    /// <summary>Arbitrary key-value metadata for tracing and external integrations</summary>
+    public Dictionary<string, object> Metadata { get; init; } = new();
+
+    /// <summary>Returns the currently soaking rollout step, or <see langword="null"/> if no step is active</summary>
+    public CanaryRolloutStep? ActiveStep =>
+        RolloutPlan.FirstOrDefault(s => s.Status == RolloutStepStatus.InProgress);
+
+    /// <summary>Returns the next pending rollout step, or <see langword="null"/> if the plan is exhausted</summary>
+    public CanaryRolloutStep? NextStep =>
+        RolloutPlan.FirstOrDefault(s => s.Status == RolloutStepStatus.Pending);
+
+    /// <summary>Returns <see langword="true"/> when every rollout step is completed or skipped</summary>
+    public bool IsRolloutComplete =>
+        RolloutPlan.Count > 0 &&
+        RolloutPlan.All(s => s.Status is RolloutStepStatus.Completed or RolloutStepStatus.Skipped);
+
+    /// <summary>Returns <see langword="true"/> when the deployment is in a terminal state and cannot be advanced</summary>
+    public bool IsTerminal =>
+        Status is CanaryStatus.Promoted or CanaryStatus.Aborted;
+
+    /// <summary>Overall rollout progress expressed as a percentage of completed steps (0–100)</summary>
+    public double ProgressPercent =>
+        RolloutPlan.Count == 0 ? 0
+        : RolloutPlan.Count(s => s.Status == RolloutStepStatus.Completed) * 100.0 / RolloutPlan.Count;
+}
+
+/// <summary>
+/// Immutable snapshot of the traffic distribution between the stable and canary versions
+/// </summary>
+public record TrafficSplit
+{
+    /// <summary>Percentage of inbound requests routed to the stable version (0–100)</summary>
+    public double StablePercent { get; init; }
+
+    /// <summary>Percentage of inbound requests routed to the canary version (0–100)</summary>
+    public double CanaryPercent { get; init; }
+
+    /// <summary>All traffic directed to stable; canary receives no requests</summary>
+    public static TrafficSplit Initial => new() { StablePercent = 100, CanaryPercent = 0 };
+
+    /// <summary>Equal 50/50 split between stable and canary</summary>
+    public static TrafficSplit Equal => new() { StablePercent = 50, CanaryPercent = 50 };
+
+    /// <summary>All traffic directed to canary; stable receives no requests</summary>
+    public static TrafficSplit FullCanary => new() { StablePercent = 0, CanaryPercent = 100 };
+
+    /// <summary>
+    /// Creates a split from the desired canary traffic percentage; stable receives the remainder
+    /// </summary>
+    /// <param name="canaryPercent">Target canary percentage in the range [0, 100]</param>
+    public static TrafficSplit FromCanaryPercent(double canaryPercent) =>
+        new()
+        {
+            CanaryPercent = Math.Round(Math.Clamp(canaryPercent, 0, 100), 2),
+            StablePercent = Math.Round(Math.Clamp(100 - canaryPercent, 0, 100), 2)
+        };
+
+    /// <inheritdoc />
+    public override string ToString() =>
+        $"Stable {StablePercent:F1}% / Canary {CanaryPercent:F1}%";
+}
+
+/// <summary>
+/// Health signal snapshot for one deployment version at a point in time
+/// </summary>
+public class CanaryMetrics
+{
+    /// <summary>Percentage of requests that resulted in an error response (4xx/5xx), range 0–100</summary>
+    public double ErrorRatePercent { get; set; }
+
+    /// <summary>95th-percentile response latency in milliseconds</summary>
+    public double P95LatencyMs { get; set; }
+
+    /// <summary>99th-percentile response latency in milliseconds</summary>
+    public double P99LatencyMs { get; set; }
+
+    /// <summary>Total number of requests observed since the current step began</summary>
+    public long RequestCount { get; set; }
+
+    /// <summary>Total number of error responses observed since the current step began</summary>
+    public long ErrorCount { get; set; }
+
+    /// <summary>UTC timestamp of the most recent metrics collection</summary>
+    public DateTime? LastUpdatedAt { get; set; }
+}
+
+/// <summary>
+/// A discrete traffic-shift step within the rollout plan
+/// </summary>
+public class CanaryRolloutStep
+{
+    /// <summary>1-based sequence number of this step within the plan</summary>
+    public int StepNumber { get; init; }
+
+    /// <summary>Target canary traffic percentage applied while this step is active (0–100)</summary>
+    public double CanaryPercent { get; init; }
+
+    /// <summary>Minimum time to observe the canary at this traffic level before the step is eligible to advance</summary>
+    public TimeSpan SoakDuration { get; init; }
+
+    /// <summary>Current progress state of this step</summary>
+    public RolloutStepStatus Status { get; set; } = RolloutStepStatus.Pending;
+
+    /// <summary>UTC timestamp when this step began soaking</summary>
+    public DateTime? StartedAt { get; set; }
+
+    /// <summary>UTC timestamp when this step was marked complete, skipped, or failed</summary>
+    public DateTime? CompletedAt { get; set; }
+
+    /// <summary>
+    /// Returns <see langword="true"/> when the configured soak duration has elapsed since the step started
+    /// </summary>
+    public bool IsSoakComplete =>
+        StartedAt.HasValue && DateTime.UtcNow - StartedAt.Value >= SoakDuration;
+}
+
+/// <summary>
+/// Result of a periodic health evaluation comparing canary metrics against stable baseline and configured thresholds
+/// </summary>
+public record CanaryEvaluationResult
+{
+    /// <summary>Returns <see langword="true"/> when no threshold violations were detected</summary>
+    public bool IsHealthy { get; init; }
+
+    /// <summary>Human-readable summary of the evaluation outcome</summary>
+    public string Reason { get; init; } = string.Empty;
+
+    /// <summary>Metrics snapshot collected from the stable version during this evaluation</summary>
+    public CanaryMetrics StableMetrics { get; init; } = new();
+
+    /// <summary>Metrics snapshot collected from the canary version during this evaluation</summary>
+    public CanaryMetrics CanaryMetrics { get; init; } = new();
+
+    /// <summary>Individual threshold violation descriptions, empty when the canary is healthy</summary>
+    public List<string> Violations { get; init; } = [];
+
+    /// <summary>
+    /// When <see langword="true"/>, the engine should trigger an automatic rollback for this deployment
+    /// </summary>
+    public bool ShouldAutoRollback { get; init; }
+
+    /// <summary>UTC timestamp when this evaluation was performed</summary>
+    public DateTime EvaluatedAt { get; init; } = DateTime.UtcNow;
+}
+
+/// <summary>
+/// Immutable request DTO for initiating a new canary deployment
+/// </summary>
+public record CanaryDeploymentRequest
+{
+    /// <summary>Project or application name</summary>
+    public required string ProjectName { get; init; }
+
+    /// <summary>Current stable version already serving 100% of production traffic</summary>
+    public required string StableVersion { get; init; }
+
+    /// <summary>New candidate version to roll out as the canary</summary>
+    public required string CanaryVersion { get; init; }
+
+    /// <summary>Target environment in which the canary rollout will execute</summary>
+    public required Environment TargetEnvironment { get; init; }
+
+    /// <summary>Traffic-splitting strategy to apply for this rollout</summary>
+    public CanaryStrategy Strategy { get; init; } = CanaryStrategy.Linear;
+
+    /// <summary>Notification channels that should receive lifecycle alerts</summary>
+    public List<NotificationChannel> NotificationChannels { get; init; } = [];
+
+    /// <summary>Priority applied to all lifecycle notifications for this deployment</summary>
+    public NotificationPriority Priority { get; init; } = NotificationPriority.High;
+
+    /// <summary>Identity of the user or automation that initiated the deployment</summary>
+    public string InitiatedBy { get; init; } = string.Empty;
+
+    /// <summary>Git branch name for the canary version</summary>
+    public string BranchName { get; init; } = string.Empty;
+
+    /// <summary>Git commit hash of the canary build artifact</summary>
+    public string CommitHash { get; init; } = string.Empty;
+
+    /// <summary>URL of the CI/CD pipeline that produced the canary artifact</summary>
+    public string BuildUrl { get; init; } = string.Empty;
+
+    /// <summary>Arbitrary metadata to attach to the deployment for tracing and integrations</summary>
+    public Dictionary<string, object> Metadata { get; init; } = new();
+}
