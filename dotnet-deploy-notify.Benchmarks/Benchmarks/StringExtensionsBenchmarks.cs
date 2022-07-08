ccdccbfa94fdc5776e28aa9d87e689a38c8dfc82
Vladyslav Zaiets <rutova2@gmail.com>
2022-07-08 07:57:27 +0300

    small StringExtensionsBenchmarks helpers

--- dotnet-deploy-notify.Benchmarks/Benchmarks/StringExtensionsBenchmarks.cs
+++ dotnet-deploy-notify.Benchmarks/Benchmarks/StringExtensionsBenchmarks.cs
@@ -3,23 +3,35 @@ using DotNetDeployNotify.Utilities;
 
 namespace DotNetDeployNotify.Benchmarks.Benchmarks;
 
+/// <summary>
+/// Benchmark class for string extension methods.
+/// </summary>
 [MemoryDiagnoser]
 public class StringExtensionsBenchmarks
 {
     private const string Input = "This is a Test-String to be processed.";
 
+    /// <summary>
+    /// Benchmark for the ToSlug method.
+    /// </summary>
     [Benchmark]
     public string ToSlug()
     {
         return Input.ToSlug();
     }
 
+    /// <summary>
+    /// Benchmark for the ToPascalCase method.
+    /// </summary>
     [Benchmark]
     public string ToPascalCase()
     {
         return Input.ToPascalCase();
     }
 
+    /// <summary>
+    /// Benchmark for the MaskSensitive method.
+    /// </summary>
     [Benchmark]
     public string MaskSensitive()
     {
