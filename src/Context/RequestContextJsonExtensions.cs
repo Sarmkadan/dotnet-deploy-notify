1343ffc67728e1296fa8c87af319db832c1b7267
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-01 18:54:40 +0300

    feat: json serialization for RequestContextJson

--- src/Context/RequestContextJsonExtensions.cs
+++ src/Context/RequestContextJsonExtensions.cs
@@ -10,87 +10,83 @@ using System.Text.Json.Serialization;
 namespace DotNetDeployNotify.Context;
 
 /// <summary>
-/// Provides System.Text.Json serialization and deserialization extensions for RequestContext
+/// Provides System.Text.Json serialization and deserialization extensions for <see cref="RequestContext"/>
 /// </summary>
 public static class RequestContextJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-        ReferenceHandler = ReferenceHandler.IgnoreCycles,
-        PropertyNameCaseInsensitive = true
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+		ReferenceHandler = ReferenceHandler.IgnoreCycles,
+		PropertyNameCaseInsensitive = true
+	};
 
-    /// <summary>
-    /// Serializes the RequestContext to a JSON string
-    /// </summary>
-    /// <param name="value">The RequestContext to serialize</param>
-    /// <param name="indented">Whether to format the JSON with indentation</param>
-    /// <returns>A JSON string representation of the RequestContext</returns>
-    public static string ToJson(this RequestContext value, bool indented = false)
-    {
-        if (value is null)
-        {
-            throw new ArgumentNullException(nameof(value));
-        }
+	/// <summary>
+	/// Serializes the <see cref="RequestContext"/> to a JSON string
+	/// </summary>
+	/// <param name="value">The <see cref="RequestContext"/> to serialize</param>
+	/// <param name="indented">Whether to format the JSON with indentation</param>
+	/// <returns>A JSON string representation of the <see cref="RequestContext"/></returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null</exception>
+	public static string ToJson(this RequestContext value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions)
-            {
-                WriteIndented = true
-            }
-            : _jsonOptions;
+		var options = indented
+			? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+			: _jsonOptions;
 
-        return JsonSerializer.Serialize(value, options);
-    }
+		return JsonSerializer.Serialize(value, options);
+	}
 
-    /// <summary>
-    /// Deserializes a RequestContext from a JSON string
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <returns>The deserialized RequestContext, or null if JSON is null or empty</returns>
-    public static RequestContext? FromJson(string json)
-    {
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return null;
-        }
+	/// <summary>
+	/// Deserializes a <see cref="RequestContext"/> from a JSON string
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize</param>
+	/// <returns>The deserialized <see cref="RequestContext"/>, or null if JSON is null, empty, or whitespace</returns>
+	/// <exception cref="JsonException">Thrown when the JSON is malformed and cannot be deserialized</exception>
+	public static RequestContext? FromJson(string json)
+	{
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return null;
+		}
 
-        try
-        {
-            return JsonSerializer.Deserialize<RequestContext>(json, _jsonOptions);
-        }
-        catch (JsonException)
-        {
-            return null;
-        }
-    }
+		try
+		{
+			return JsonSerializer.Deserialize<RequestContext>(json, _jsonOptions);
+		}
+		catch (JsonException ex)
+		{
+			throw new JsonException("Failed to deserialize RequestContext from JSON", ex);
+		}
+	}
 
-    /// <summary>
-    /// Attempts to deserialize a RequestContext from a JSON string
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <param name="value">Receives the deserialized RequestContext if successful</param>
-    /// <returns>True if deserialization succeeded; otherwise, false</returns>
-    public static bool TryFromJson(string json, out RequestContext? value)
-    {
-        value = null;
+	/// <summary>
+	/// Attempts to deserialize a <see cref="RequestContext"/> from a JSON string
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize</param>
+	/// <param name="value">Receives the deserialized <see cref="RequestContext"/> if successful</param>
+	/// <returns>True if deserialization succeeded; otherwise, false</returns>
+	public static bool TryFromJson(string json, out RequestContext? value)
+	{
+		value = null;
 
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return false;
-        }
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return false;
+		}
 
-        try
-        {
-            value = JsonSerializer.Deserialize<RequestContext>(json, _jsonOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            return false;
-        }
-    }
+		try
+		{
+			value = JsonSerializer.Deserialize<RequestContext>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			return false;
+		}
+	}
 }
