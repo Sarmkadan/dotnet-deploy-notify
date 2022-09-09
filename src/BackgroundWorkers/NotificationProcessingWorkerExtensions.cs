8e07fb851ea8d88b66f840d05bbb7cf6c8e592a8
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-09 09:32:55 +0300

    feat(backgroundworkers): extension methods on NotificationProcessingWork

--- src/BackgroundWorkers/NotificationProcessingWorkerExtensions.cs
+++ src/BackgroundWorkers/NotificationProcessingWorkerExtensions.cs
@@ -24,10 +24,10 @@ namespace DotNetDeployNotify.BackgroundWorkers
         /// <param name="worker">The notification processing worker instance.</param>
         /// <param name="interval">The interval at which to process notifications.</param>
         /// <returns>The configured notification processing worker.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="worker"/> is <see langword="null"/>.</exception>
         public static NotificationProcessingWorker WithInterval(this NotificationProcessingWorker worker, TimeSpan interval)
         {
-            if (worker == null)
-                throw new ArgumentNullException(nameof(worker));
+            ArgumentNullException.ThrowIfNull(worker);
 
             // Note: In a real implementation, we would need to access the private _interval field
             // For this extension method, we'll document that this is a configuration method
@@ -41,12 +41,11 @@ namespace DotNetDeployNotify.BackgroundWorkers
         /// <param name="worker">The notification processing worker instance.</param>
         /// <param name="logger">The logger instance for detailed logging.</param>
         /// <returns>The notification processing worker with detailed logging enabled.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="worker"/> or <paramref name="logger"/> is <see langword="null"/>.</exception>
         public static NotificationProcessingWorker WithDetailedLogging(this NotificationProcessingWorker worker, ILogger logger)
         {
-            if (worker == null)
-                throw new ArgumentNullException(nameof(worker));
-            if (logger == null)
-                throw new ArgumentNullException(nameof(logger));
+            ArgumentNullException.ThrowIfNull(worker);
+            ArgumentNullException.ThrowIfNull(logger);
 
             // This extension method documents the intended usage pattern
             // In a real implementation, we would configure the worker's logger
@@ -59,12 +58,11 @@ namespace DotNetDeployNotify.BackgroundWorkers
         /// <param name="worker">The notification processing worker instance.</param>
         /// <param name="logger">The logger instance for health check messages.</param>
         /// <returns>A <see cref="ScheduledTask"/> that monitors the worker's health.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="worker"/> or <paramref name="logger"/> is <see langword="null"/>.</exception>
         public static ScheduledTask CreateHealthCheckTask(this NotificationProcessingWorker worker, ILogger logger)
         {
-            if (worker == null)
-                throw new ArgumentNullException(nameof(worker));
-            if (logger == null)
-                throw new ArgumentNullException(nameof(logger));
+            ArgumentNullException.ThrowIfNull(worker);
+            ArgumentNullException.ThrowIfNull(logger);
 
             return new NotificationProcessingHealthCheckTask(logger, worker);
         }
@@ -74,14 +72,14 @@ namespace DotNetDeployNotify.BackgroundWorkers
         /// </summary>
         /// <param name="worker">The notification processing worker instance.</param>
         /// <returns>A tuple containing the worker's statistics: total notifications processed, success rate, and uptime.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="worker"/> is <see langword="null"/>.</exception>
         public static (int TotalProcessed, double SuccessRate, TimeSpan Uptime) GetStatistics(this NotificationProcessingWorker worker)
         {
-            if (worker == null)
-                throw new ArgumentNullException(nameof(worker));
+            ArgumentNullException.ThrowIfNull(worker);
 
             // In a real implementation, this would track and return actual statistics
             // For now, return default values as this is a placeholder for actual implementation
-            return (0, 0.0, TimeSpan.Zero);
+        return (0, 0.0, TimeSpan.Zero);
         }
     }
 
@@ -98,6 +96,7 @@ namespace DotNetDeployNotify.BackgroundWorkers
         /// </summary>
         /// <param name="logger">The logger instance.</param>
         /// <param name="worker">The notification processing worker to monitor.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="logger"/> or <paramref name="worker"/> is <see langword="null"/>.</exception>
         public NotificationProcessingHealthCheckTask(ILogger logger, NotificationProcessingWorker worker)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
@@ -111,17 +110,27 @@ namespace DotNetDeployNotify.BackgroundWorkers
         /// Executes the health check for the notification processing worker.
         /// </summary>
         /// <returns>A task representing the asynchronous operation.</returns>
+        /// <exception cref="Exception">Throws if the health check fails to complete.</exception>
         public override async Task ExecuteAsync()
         {
-            _logger.LogInformation("Running health check for notification processing worker...");
+            try
+            {
+                _logger.LogInformation("Running health check for notification processing worker...");
 
-            // Check if worker is running
-            var workerType = _worker.GetType().Name;
-            _logger.LogInformation("Worker type: {WorkerType}", workerType);
+                // Check if worker is running
+                var workerType = _worker.GetType().Name;
+                _logger.LogInformation("Worker type: {WorkerType}", workerType);
 
-            // In a real implementation, we would check actual worker state
-            // For now, just log the check
-            await Task.CompletedTask;
+                // In a real implementation, we would check actual worker state and statistics
+                var stats = _worker.GetStatistics();
+                _logger.LogInformation("Worker statistics - Total processed: {TotalProcessed}, Success rate: {SuccessRate:P}, Uptime: {Uptime}",
+                    stats.TotalProcessed, stats.SuccessRate, stats.Uptime);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to complete health check for notification processing worker");
+                throw;
+            }
         }
     }
 }
