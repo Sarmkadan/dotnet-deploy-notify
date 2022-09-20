f0232ffad654c437916204d09eb84a2cd65c327e
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-20 08:17:50 +0300

    feat(benchmarks): json round-trip support for CacheBenchmarksJson.

---
+++ dotnet-deploy-notify.Benchmarks/Benchmarks/CacheBenchmarksJsonExtensions.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotNetDeployNotify.Benchmarks.Benchmarks;
+
+/// <summary>
+/// Provides JSON serialization helpers for <see cref="CacheBenchmarks"/>.
+/// </summary>
+public static class CacheBenchmarksJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+    /// <summary>
+    /// Converts the specified <see cref="CacheBenchmarks"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The <see cref="CacheBenchmarks"/> instance to convert.</param>
+    /// <param name="indented">Whether to format the JSON string with indentation.</param>
+    /// <returns>A JSON string representation of the <see cref="CacheBenchmarks"/> instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+    public static string ToJson(this CacheBenchmarks value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        return JsonSerializer.Serialize(value, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a <see cref="CacheBenchmarks"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A <see cref="CacheBenchmarks"/> instance if deserialization is successful; otherwise, null.</returns>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="json"/> is null or empty.</exception>
+    public static CacheBenchmarks? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+        try
+        {
+            return JsonSerializer.Deserialize<CacheBenchmarks>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a <see cref="CacheBenchmarks"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized <see cref="CacheBenchmarks"/> instance if successful; otherwise, null.</param>
+    /// <returns>True if deserialization is successful; otherwise, false.</returns>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJson(string json, out CacheBenchmarks? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+        try
+        {
+            value = JsonSerializer.Deserialize<CacheBenchmarks>(json, _jsonSerializerOptions);
+            return value is not null;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
