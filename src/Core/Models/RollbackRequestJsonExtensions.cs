3e432ccec99d2a22f9e503f99a21de4a7c3acb92
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-23 13:30:22 +0300

    tidy up RollbackRequestJson json handling

---
+++ src/Core/Models/RollbackRequestJsonExtensions.cs
@@ -0,0 +1,71 @@
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace DotNetDeployNotify.Core.Models;
+
+public static class RollbackRequestJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+        WriteIndented = false
+    };
+
+    private static readonly JsonSerializerOptions _jsonOptionsIndented = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+        WriteIndented = true
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="RollbackRequest"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The rollback request to serialize.</param>
+    /// <param name="indented">Whether to indent the JSON output for readability.</param>
+    /// <returns>A JSON string representation of the rollback request.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+    public static string ToJson(this RollbackRequest value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, indented ? _jsonOptionsIndented : _jsonOptions);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string into a <see cref="RollbackRequest"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized rollback request, or null if the JSON is null or empty.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is null.</exception>
+    /// <exception cref="JsonException">Thrown if the JSON is invalid or cannot be deserialized.</exception>
+    public static RollbackRequest? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        return JsonSerializer.Deserialize<RollbackRequest>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a <see cref="RollbackRequest"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized rollback request if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out RollbackRequest? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<RollbackRequest>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
