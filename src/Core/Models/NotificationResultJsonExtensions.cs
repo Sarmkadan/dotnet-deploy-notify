6c2d70fc7cb4d1617f1e2d911fbf20e1b9a3c985
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-24 11:28:36 +0300

    Add NotificationResultJson json helpers

---
+++ src/Core/Models/NotificationResultJsonExtensions.cs
@@ -0,0 +1,88 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotNetDeployNotify.Core.Models;
+
+/// <summary>
+/// Provides JSON serialization extensions for <see cref="NotificationResult"/> objects
+/// </summary>
+public static class NotificationResultJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    private static readonly JsonSerializerOptions _jsonOptionsIndented = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Converts a <see cref="NotificationResult"/> object to its JSON representation
+    /// </summary>
+    /// <param name="value">The notification result to serialize</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability</param>
+    /// <returns>JSON string representation of the notification result</returns>
+    /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+    public static string ToJson(this NotificationResult value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, indented
+            ? _jsonOptionsIndented
+            : _jsonOptions);
+    }
+
+    /// <summary>
+    /// Parses a JSON string into a <see cref="NotificationResult"/> object
+    /// </summary>
+    /// <param name="json">JSON string to deserialize</param>
+    /// <returns>Deserialized notification result, or null if JSON is empty or whitespace</returns>
+    /// <exception cref="ArgumentException">Thrown if json is null or empty</exception>
+    /// <exception cref="JsonException">Thrown when JSON is invalid or cannot be deserialized</exception>
+    public static NotificationResult? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<NotificationResult>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to parse a JSON string into a <see cref="NotificationResult"/> object
+    /// </summary>
+    /// <param name="json">JSON string to deserialize</param>
+    /// <param name="value">Receives the deserialized notification result if successful</param>
+    /// <returns>True if deserialization succeeded; false otherwise</returns>
+    /// <exception cref="ArgumentException">Thrown if json is null or empty</exception>
+    public static bool TryFromJson(string json, out NotificationResult? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<NotificationResult>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
