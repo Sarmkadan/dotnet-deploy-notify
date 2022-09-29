67ae847b7379c33fa4a6d8a4c5b65bbf5cb203f3
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-29 16:52:43 +0300

    Tidy up MathExtensionsJson json handling.
    will circle back to tests later.

---
+++ src/Utilities/MathExtensionsJsonExtensions.cs
@@ -0,0 +1,132 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotNetDeployNotify.Utilities;
+
+/// <summary>
+/// Provides System.Text.Json serialization helpers for <see cref="MathExtensions"/>
+/// </summary>
+public static class MathExtensionsJsonExtensions
+{
+    /// <summary>
+    /// JsonSerializerOptions with camelCase naming policy for consistent serialization
+    /// </summary>
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.General)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNameCaseInsensitive = true
+    };
+
+    /// <summary>
+    /// Serializes MathExtensions type information to JSON string
+    /// </summary>
+    /// <param name="indented">Whether to format the JSON with indentation</param>
+    /// <returns>JSON string representation of MathExtensions metadata</returns>
+    public static string ToJson(bool indented = false)
+    {
+        var options = new JsonSerializerOptions(_jsonOptions)
+        {
+            WriteIndented = indented
+        };
+
+        var metadata = new MathExtensionsMetadata
+        {
+            Type = nameof(MathExtensions),
+            Namespace = typeof(MathExtensions).Namespace ?? "DotNetDeployNotify.Utilities",
+            Assembly = typeof(MathExtensions).Assembly.GetName().Name ?? "DotNetDeployNotify",
+            Methods = [
+                "Clamp",
+                "IsBetween",
+                "ToPercentage",
+                "RoundTo",
+                "Average",
+                "Median",
+                "SafeSum",
+                "SafeAverage",
+                "ToHumanReadableSize",
+                "ToHumanReadableDuration",
+                "CalculateCompoundInterest",
+                "RandomBetween"
+            ]
+        };
+
+        return JsonSerializer.Serialize(metadata, options);
+    }
+
+    /// <summary>
+    /// Deserializes JSON string to MathExtensions metadata
+    /// </summary>
+    /// <param name="json">JSON string to deserialize</param>
+    /// <returns>MathExtensions metadata or null if deserialization fails</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null</exception>
+    public static MathExtensionsMetadata? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        try
+        {
+            return JsonSerializer.Deserialize<MathExtensionsMetadata>(json, _jsonOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize JSON string to MathExtensions metadata
+    /// </summary>
+    /// <param name="json">JSON string to deserialize</param>
+    /// <param name="value">Output value, null if deserialization fails</param>
+    /// <returns>True if deserialization succeeds, false otherwise</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null</exception>
+    public static bool TryFromJson(string json, out MathExtensionsMetadata? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<MathExtensionsMetadata>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Metadata representation of the MathExtensions class for JSON serialization
+    /// </summary>
+    public sealed class MathExtensionsMetadata
+    {
+        /// <summary>
+        /// Gets or sets the type identifier
+        /// </summary>
+        public string? Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the namespace
+        /// </summary>
+        public string? Namespace { get; set; }
+
+        /// <summary>
+        /// Gets or sets the assembly name
+        /// </summary>
+        public string? Assembly { get; set; }
+
+        /// <summary>
+        /// Gets or sets the array of method names
+        /// </summary>
+        public string[]? Methods { get; set; }
+    }
+}
