3226c1e3e7a9e3d5b5d4021ee28d2183c5f17c40
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-30 11:36:46 +0300

    feat: serialize GuardExtensionsJson properly

---
+++ src/Utilities/GuardExtensionsJsonExtensions.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotNetDeployNotify.Utilities
+{
+    /// <summary>
+    /// Provides System.Text.Json serialization extensions for <see cref="GuardExtensions"/>
+    /// </summary>
+    public static class GuardExtensionsJsonExtensions
+    {
+        /// <summary>
+        /// JsonSerializerOptions with camelCase naming policy for consistent serialization
+        /// </summary>
+        private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.General)
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            PropertyNameCaseInsensitive = true
+        };
+
+        /// <summary>
+        /// Serializes GuardExtensions type information to JSON string
+        /// </summary>
+        /// <param name="indented">Whether to format the JSON with indentation</param>
+        /// <returns>JSON string representation of GuardExtensions metadata</returns>
+        public static string ToJson(bool indented = false)
+        {
+            var options = new JsonSerializerOptions(_jsonOptions)
+            {
+                WriteIndented = indented,
+            };
+
+            var metadata = new GuardExtensionsMetadata
+            {
+                Type = nameof(GuardExtensions),
+                Namespace = typeof(GuardExtensions).Namespace ?? "DotNetDeployNotify.Utilities",
+                Assembly = typeof(GuardExtensions).Assembly.GetName().Name ?? "DotNetDeployNotify",
+                Methods = [
+                    "ThrowIfNull",
+                    "ThrowIfNullOrEmpty",
+                    "ThrowIfFalse",
+                    "ThrowIfLessThan",
+                    "ThrowIfLongerThan",
+                    "ThrowIfInvalidUrl",
+                    "GetValueOrThrow",
+                    "IsInRange",
+                    "MatchesPattern"
+                ],
+            };
+
+            return JsonSerializer.Serialize(metadata, options);
+        }
+
+        /// <summary>
+        /// Deserializes JSON string to GuardExtensions metadata
+        /// </summary>
+        /// <param name="json">JSON string to deserialize</param>
+        /// <returns>GuardExtensions metadata or null if deserialization fails</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null</exception>
+        public static GuardExtensionsMetadata? FromJson(string json)
+        {
+            ArgumentNullException.ThrowIfNull(json);
+
+            try
+            {
+                return JsonSerializer.Deserialize<GuardExtensionsMetadata>(json, _jsonOptions);
+            }
+            catch (JsonException)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to deserialize JSON string to GuardExtensions metadata
+        /// </summary>
+        /// <param name="json">JSON string to deserialize</param>
+        /// <param name="value">Output value, null if deserialization fails</param>
+        /// <returns>True if deserialization succeeds, false otherwise</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null</exception>
+        public static bool TryFromJson(string json, out GuardExtensionsMetadata? value)
+        {
+            ArgumentNullException.ThrowIfNull(json);
+
+            try
+            {
+                value = JsonSerializer.Deserialize<GuardExtensionsMetadata>(json, _jsonOptions);
+                return true;
+            }
+            catch (JsonException)
+            {
+                value = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Metadata representation of the GuardExtensions class for JSON serialization
+        /// </summary>
+        public sealed class GuardExtensionsMetadata
+        {
+            /// <summary>
+            /// Gets or sets the type identifier
+            /// </summary>
+            public string? Type { get; set; }
+
+            /// <summary>
+            /// Gets or sets the namespace
+            /// </summary>
+            public string? Namespace { get; set; }
+
+            /// <summary>
+            /// Gets or sets the assembly name
+            /// </summary>
+            public string? Assembly { get; set; }
+
+            /// <summary>
+            /// Gets or sets the array of method names
+            /// </summary>
+            public string[]? Methods { get; set; }
+        }
+    }
+}
