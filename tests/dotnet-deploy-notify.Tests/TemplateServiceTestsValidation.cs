3121cc96c3980c8cf230b68f68e52da802e2cd9f
Vladyslav Zaiets <rutova2@gmail.com>
2022-10-31 20:25:26 +0200

    validate TemplateService args.

---
+++ tests/dotnet-deploy-notify.Tests/TemplateServiceTestsValidation.cs
@@ -0,0 +1,61 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Xunit;
+
+namespace DotNetDeployNotify.Tests;
+
+/// <summary>
+/// Provides validation helpers for <see cref="TemplateServiceTests"/> to ensure test data integrity and configuration validity.
+/// </summary>
+public static class TemplateServiceTestsValidation
+{
+    /// <summary>
+    /// Validates the specified <see cref="TemplateServiceTests"/> instance for common issues.
+    /// </summary>
+    /// <param name="value">The template service tests instance to validate.</param>
+    /// <returns>A list of human-readable validation problems; empty if valid.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static IReadOnlyList<string> Validate(this TemplateServiceTests? value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var problems = new List<string>();
+
+        // TemplateServiceTests is a test class with no data members to validate
+        // All validation is structural (compiler ensures the class exists and has the expected methods)
+        // The actual test methods are validated by the compiler and test runner
+
+        return problems.AsReadOnly();
+    }
+
+    /// <summary>
+    /// Determines whether the specified <see cref="TemplateServiceTests"/> instance is valid.
+    /// </summary>
+    /// <param name="value">The template service tests instance to check.</param>
+    /// <returns>True if the instance is valid; otherwise, false.</returns>
+    public static bool IsValid(this TemplateServiceTests? value)
+    {
+        return Validate(value).Count == 0;
+    }
+
+    /// <summary>
+    /// Ensures that the specified <see cref="TemplateServiceTests"/> instance is valid, throwing an exception if not.
+    /// </summary>
+    /// <param name="value">The template service tests instance to validate.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when validation fails, containing a list of problems.</exception>
+    public static void EnsureValid(this TemplateServiceTests? value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var problems = Validate(value);
+        if (problems.Count > 0)
+        {
+            throw new ArgumentException(
+                $"TemplateServiceTests validation failed:{Environment.NewLine} - {string.Join($"{Environment.NewLine} - ", problems)}");
+        }
+    }
+}
