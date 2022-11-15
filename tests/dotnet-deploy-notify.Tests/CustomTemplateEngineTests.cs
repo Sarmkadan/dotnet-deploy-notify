7bcad897d594ebeef0a2ec2aa134961790b91dc2
Vladyslav Zaiets <rutova2@gmail.com>
2022-11-15 08:02:32 +0200

    minor

--- tests/dotnet-deploy-notify.Tests/CustomTemplateEngineTests.cs
+++ tests/dotnet-deploy-notify.Tests/CustomTemplateEngineTests.cs
@@ -10,10 +10,18 @@ using Xunit;
 
 namespace DotNetDeployNotify.Tests;
 
+/// <summary>
+/// Unit tests for the <see cref="CustomTemplateEngine"/> class.
+/// Tests template registration, retrieval, rendering, validation, and preset loading functionality.
+/// </summary>
 public class CustomTemplateEngineTests
 {
     private readonly CustomTemplateEngine _engine;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CustomTemplateEngineTests"/> class.
+    /// Sets up a mocked logger and creates a <see cref="CustomTemplateEngine"/> instance for testing.
+    /// </summary>
     public CustomTemplateEngineTests()
     {
         var logger = Substitute.For<ILogger<CustomTemplateEngine>>();
@@ -22,6 +30,10 @@ public class CustomTemplateEngineTests
 
     // ─── RegisterTemplate / GetTemplate ────────────────────────────────────
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RegisterTemplate(CustomTemplate)"/> correctly stores a template
+    /// and that <see cref="CustomTemplateEngine.GetTemplate(string)"/> can retrieve it.
+    /// </summary>
     [Fact]
     public void RegisterTemplate_StoresTemplate()
     {
@@ -30,6 +42,10 @@ public class CustomTemplateEngineTests
         _engine.GetTemplate("MyTpl").Should().NotBeNull();
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RegisterTemplate(CustomTemplate)"/> overwrites an existing template
+    /// with the same name, ensuring the latest version is stored.
+    /// </summary>
     [Fact]
     public void RegisterTemplate_OverwritesExisting()
     {
@@ -39,6 +55,10 @@ public class CustomTemplateEngineTests
         _engine.GetTemplate("Tpl")!.Content.Should().Be("v2 {{ProjectName}}");
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RegisterTemplate(CustomTemplate)"/> throws an <see cref="ArgumentNullException"/>
+    /// when a null template is provided.
+    /// </summary>
     [Fact]
     public void RegisterTemplate_WithNullTemplate_ThrowsArgumentNullException()
     {
@@ -46,6 +66,10 @@ public class CustomTemplateEngineTests
         act.Should().Throw<ArgumentNullException>();
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RegisterTemplate(CustomTemplate)"/> throws an <see cref="ArgumentException"/>
+    /// when a template with an empty name is provided.
+    /// </summary>
     [Fact]
     public void RegisterTemplate_WithEmptyName_ThrowsArgumentException()
     {
@@ -54,12 +78,20 @@ public class CustomTemplateEngineTests
         act.Should().Throw<ArgumentException>();
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.GetTemplate(string)"/> returns null when querying for a template
+    /// that does not exist in the engine's registry.
+    /// </summary>
     [Fact]
     public void GetTemplate_UnknownName_ReturnsNull()
     {
         _engine.GetTemplate("DoesNotExist").Should().BeNull();
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.GetTemplate(string)"/> performs case-insensitive template name matching,
+    /// allowing retrieval of templates regardless of name casing.
+    /// </summary>
     [Fact]
     public void GetTemplate_IsCaseInsensitive()
     {
@@ -69,6 +101,10 @@ public class CustomTemplateEngineTests
 
     // ─── ListTemplates ──────────────────────────────────────────────────────
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.ListTemplates()"/> returns all registered active templates,
+    /// excluding any that have been deleted.
+    /// </summary>
     [Fact]
     public void ListTemplates_ReturnsAllActive()
     {
@@ -78,6 +114,10 @@ public class CustomTemplateEngineTests
         _engine.ListTemplates().Should().HaveCount(2);
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.ListTemplates()"/> excludes deleted templates from the returned list,
+    /// ensuring only active templates are included.
+    /// </summary>
     [Fact]
     public void ListTemplates_ExcludesDeletedTemplates()
     {
@@ -90,6 +130,9 @@ public class CustomTemplateEngineTests
 
     // ─── DeleteTemplate ──────────────────────────────────────────────────────
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.DeleteTemplate(string)"/> returns true when successfully deleting an existing template.
+    /// </summary>
     [Fact]
     public void DeleteTemplate_ReturnsTrueForExisting()
     {
@@ -97,30 +140,46 @@ public class CustomTemplateEngineTests
         _engine.DeleteTemplate("TplDel").Should().BeTrue();
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.DeleteTemplate(string)"/> returns false when attempting to delete a template
+    /// that does not exist in the registry.
+    /// </summary>
     [Fact]
     public void DeleteTemplate_ReturnsFalseForUnknown()
     {
         _engine.DeleteTemplate("Ghost").Should().BeFalse();
     }
 
-    // ─── RenderInline – variable substitution ───────────────────────────────
+    // ─── RenderInline – variable substitution ───────────────────────────────────
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification)"/> correctly replaces the "ProjectName" variable
+    /// in the template with the actual project name from the deployment notification.
+    /// </summary>
     [Fact]
     public void RenderInline_ReplacesProjectName()
     {
         var notification = CreateNotification();
         _engine.RenderInline("Project: {{ProjectName}}", notification)
-               .Should().Be("Project: TestApp");
+            .Should().Be("Project: TestApp");
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification)"/> correctly replaces the "Version" variable
+    /// in the template with the actual version from the deployment notification.
+    /// </summary>
     [Fact]
     public void RenderInline_ReplacesVersion()
     {
         var notification = CreateNotification();
         _engine.RenderInline("v{{Version}}", notification)
-               .Should().Be("v2.0.0");
+            .Should().Be("v2.0.0");
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification)"/> correctly replaces multiple variables
+    /// in the template with their corresponding values from the deployment notification.
+    /// </summary>
     [Fact]
     public void RenderInline_ReplacesMultipleVariables()
     {
@@ -129,55 +188,79 @@ public class CustomTemplateEngineTests
         result.Should().Be("TestApp v2.0.0 [Success]");
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification)"/> leaves unknown variables unchanged
+    /// in the output when they are not defined in the deployment notification.
+    /// </summary>
     [Fact]
     public void RenderInline_LeavesUnknownVariablesUnchanged()
     {
         var notification = CreateNotification();
         _engine.RenderInline("{{Unknown}}", notification)
-               .Should().Be("{{Unknown}}");
+            .Should().Be("{{Unknown}}");
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification)"/> returns an empty string
+    /// when an empty template is provided.
+    /// </summary>
     [Fact]
     public void RenderInline_WithEmptyTemplate_ReturnsEmpty()
     {
         _engine.RenderInline(string.Empty, CreateNotification())
-               .Should().BeEmpty();
+            .Should().BeEmpty();
     }
 
     // ─── RenderInline – filters ─────────────────────────────────────────────
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification)"/> applies the "upper" filter
+    /// to convert the project name to uppercase.
+    /// </summary>
     [Fact]
     public void RenderInline_UpperFilter_ConvertsToUpperCase()
     {
         var notification = CreateNotification();
         _engine.RenderInline("{{ProjectName | upper}}", notification)
-               .Should().Be("TESTAPP");
+            .Should().Be("TESTAPP");
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification)"/> applies the "lower" filter
+    /// to convert the status to lowercase.
+    /// </summary>
     [Fact]
     public void RenderInline_LowerFilter_ConvertsToLowerCase()
     {
         var notification = CreateNotification();
         _engine.RenderInline("{{Status | lower}}", notification)
-               .Should().Be("success");
+            .Should().Be("success");
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification)"/> applies the "trim" filter
+    /// to remove leading and trailing whitespace from the project name.
+    /// </summary>
     [Fact]
     public void RenderInline_TrimFilter_TrimsWhitespace()
     {
         var notification = new DeploymentNotification
         {
-            ProjectName = "  SpacedApp  ",
+            ProjectName = " SpacedApp ",
             Version = "1.0",
             BranchName = "main",
             Channels = [NotificationChannel.Slack]
         };
         _engine.RenderInline("'{{ProjectName | trim}}'", notification)
-               .Should().Be("'SpacedApp'");
+            .Should().Be("'SpacedApp'");
     }
 
     // ─── RenderInline – custom variables ────────────────────────────────────
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification, IDictionary{string, string})"/> allows custom variables
+    /// to override built-in variables like "ProjectName".
+    /// </summary>
     [Fact]
     public void RenderInline_CustomVariable_OverridesBuiltin()
     {
@@ -185,9 +268,13 @@ public class CustomTemplateEngineTests
         var custom = new Dictionary<string, string> { ["ProjectName"] = "OverriddenApp" };
 
         _engine.RenderInline("{{ProjectName}}", notification, custom)
-               .Should().Be("OverriddenApp");
+            .Should().Be("OverriddenApp");
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification, IDictionary{string, string})"/> includes custom variables
+    /// that are not defined in the deployment notification.
+    /// </summary>
     [Fact]
     public void RenderInline_CustomVariable_AppearsInOutput()
     {
@@ -195,11 +282,15 @@ public class CustomTemplateEngineTests
         var custom = new Dictionary<string, string> { ["Deployer"] = "jenkins-bot" };
 
         _engine.RenderInline("Deployed by {{Deployer}}", notification, custom)
-               .Should().Be("Deployed by jenkins-bot");
+            .Should().Be("Deployed by jenkins-bot");
     }
 
     // ─── RenderInline – conditional blocks ──────────────────────────────────
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification)"/> renders conditional blocks
+    /// when the condition matches the actual status.
+    /// </summary>
     [Fact]
     public void RenderInline_ConditionalBlock_ShowsWhenMatches()
     {
@@ -213,6 +304,10 @@ public class CustomTemplateEngineTests
         result.Should().Contain("ALERT: build broken");
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification)"/> hides conditional blocks
+    /// when the condition does not match the actual status.
+    /// </summary>
     [Fact]
     public void RenderInline_ConditionalBlock_HidesWhenNotMatches()
     {
@@ -226,6 +321,10 @@ public class CustomTemplateEngineTests
         result.Trim().Should().BeEmpty();
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.RenderInline(string, DeploymentNotification)"/> performs case-insensitive comparison
+    /// for conditional blocks.
+    /// </summary>
     [Fact]
     public void RenderInline_ConditionalBlock_CaseInsensitiveComparison()
     {
@@ -241,6 +340,10 @@ public class CustomTemplateEngineTests
 
     // ─── Render (named template) ────────────────────────────────────────────
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.Render(string, DeploymentNotification)"/> successfully renders a named template
+    /// when it exists in the registry.
+    /// </summary>
     [Fact]
     public void Render_KnownTemplate_ReturnsRenderedContent()
     {
@@ -248,9 +351,13 @@ public class CustomTemplateEngineTests
         var notification = CreateNotification();
 
         _engine.Render("Greeting", notification)
-               .Should().Be("Hello from TestApp");
+            .Should().Be("Hello from TestApp");
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.Render(string, DeploymentNotification)"/> throws a <see cref="KeyNotFoundException"/>
+    /// when attempting to render a template that does not exist.
+    /// </summary>
     [Fact]
     public void Render_UnknownTemplate_ThrowsKeyNotFoundException()
     {
@@ -258,6 +365,10 @@ public class CustomTemplateEngineTests
         act.Should().Throw<KeyNotFoundException>();
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.Render(string, DeploymentNotification)"/> throws a <see cref="KeyNotFoundException"/>
+    /// when attempting to render a template that has been deleted.
+    /// </summary>
     [Fact]
     public void Render_DeletedTemplate_ThrowsKeyNotFoundException()
     {
@@ -270,6 +381,9 @@ public class CustomTemplateEngineTests
 
     // ─── ValidateTemplate ───────────────────────────────────────────────────
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.ValidateTemplate(string)"/> returns valid for a properly formatted template.
+    /// </summary>
     [Fact]
     public void ValidateTemplate_ValidTemplate_ReturnsNoErrors()
     {
@@ -278,6 +392,10 @@ public class CustomTemplateEngineTests
         errors.Should().BeEmpty();
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.ValidateTemplate(string)"/> detects unknown variables
+    /// and returns appropriate error messages.
+    /// </summary>
     [Fact]
     public void ValidateTemplate_UnknownVariable_ReturnsError()
     {
@@ -286,6 +404,10 @@ public class CustomTemplateEngineTests
         errors.Should().ContainSingle(e => e.Contains("UnknownVar"));
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.ValidateTemplate(string)"/> detects unknown filters
+    /// and returns appropriate error messages.
+    /// </summary>
     [Fact]
     public void ValidateTemplate_UnknownFilter_ReturnsError()
     {
@@ -294,6 +416,10 @@ public class CustomTemplateEngineTests
         errors.Should().ContainSingle(e => e.Contains("explode"));
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.ValidateTemplate(string)"/> detects unbalanced conditional blocks
+    /// and returns appropriate error messages.
+    /// </summary>
     [Fact]
     public void ValidateTemplate_UnbalancedConditional_ReturnsError()
     {
@@ -302,6 +428,9 @@ public class CustomTemplateEngineTests
         errors.Should().ContainSingle(e => e.Contains("Unbalanced"));
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.ValidateTemplate(string)"/> considers an empty template as valid.
+    /// </summary>
     [Fact]
     public void ValidateTemplate_EmptyTemplate_IsValid()
     {
@@ -312,6 +441,10 @@ public class CustomTemplateEngineTests
 
     // ─── LoadPresets ────────────────────────────────────────────────────────
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.LoadPresets()"/> registers built-in templates
+    /// including SuccessAlert and FailureAlert.
+    /// </summary>
     [Fact]
     public void LoadPresets_RegistersBuiltinTemplates()
     {
@@ -322,6 +455,10 @@ public class CustomTemplateEngineTests
         _engine.GetTemplate("FailureAlert").Should().NotBeNull();
     }
 
+    /// <summary>
+    /// Tests that <see cref="CustomTemplateEngine.LoadPresets()"/> registers templates that can be rendered
+    /// without throwing exceptions for valid deployment notifications.
+    /// </summary>
     [Fact]
     public void LoadPresets_PresetsRenderWithoutErrors()
     {
