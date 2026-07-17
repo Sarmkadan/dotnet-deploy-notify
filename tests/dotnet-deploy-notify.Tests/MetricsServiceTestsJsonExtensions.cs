b69a8240dbe854ded98993d649b4bed98b62579d
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Cleanup in MetircsServiceTestsJsonExtensions
    will circle back to tests later.

--- tests/dotnet-deploy-notify.Tests/MetricsServiceTestsJsonExtensions.cs
+++ tests/dotnet-deploy-notify.Tests/MetricsServiceTestsJsonExtensions.cs
@@ -2,27 +2,51 @@ using System.Text.Json;
 
 namespace DotNetDeployNotify.Tests;
 
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="MetricsServiceTests"/> instances
+/// used in unit testing scenarios.
+/// </summary>
 public static class MetricsServiceTestsJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new()
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         WriteIndented = false
     };
 
+    /// <summary>
+    /// Serializes a <see cref="MetricsServiceTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the instance.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this MetricsServiceTests value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
+
         if (indented)
         {
             _jsonSerializerOptions.WriteIndented = true;
         }
+        else
+        {
+            _jsonSerializerOptions.WriteIndented = false;
+        }
+
         return JsonSerializer.Serialize(value, _jsonSerializerOptions);
     }
 
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="MetricsServiceTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized instance, or <see langword="null"/> if deserialization fails.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
     public static MetricsServiceTests? FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
+
         try
         {
             return JsonSerializer.Deserialize<MetricsServiceTests>(json, _jsonSerializerOptions);
@@ -33,9 +57,17 @@ public static class MetricsServiceTestsJsonExtensions
         }
     }
 
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="MetricsServiceTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
     public static bool TryFromJson(string json, out MetricsServiceTests? value)
     {
         ArgumentNullException.ThrowIfNull(json);
+
         try
         {
             value = JsonSerializer.Deserialize<MetricsServiceTests>(json, _jsonSerializerOptions);
