726f6a766b051dcf55f7582d5c7b3eca69c2cb73
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Cleanup in NotificationTestsExtensions

--- tests/dotnet-deploy-notify.Tests/NotificationTestsExtensions.cs
+++ tests/dotnet-deploy-notify.Tests/NotificationTestsExtensions.cs
@@ -15,9 +15,9 @@ public static class NotificationTestsExtensions
     /// <summary>
     /// Creates a new <see cref="NotificationBuilder"/> instance.
     /// </summary>
-    /// <param name="tests">The test instance</param>
-    /// <exception cref="ArgumentNullException">Thrown when tests is null.</exception>
-    /// <returns>A new NotificationBuilder instance.</returns>
+    /// <param name="tests">The test instance.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/>.</exception>
+    /// <returns>A new <see cref="NotificationBuilder"/> instance.</returns>
     public static NotificationBuilder CreateBuilder(this NotificationTests tests)
     {
         ArgumentNullException.ThrowIfNull(tests);
@@ -27,12 +27,12 @@ public static class NotificationTestsExtensions
     /// <summary>
     /// Creates a valid <see cref="DeploymentNotification"/> for testing purposes.
     /// </summary>
-    /// <param name="tests">The test instance</param>
-    /// <param name="projectName">The project name</param>
-    /// <param name="version">The version string</param>
-    /// <exception cref="ArgumentNullException">Thrown when tests is null.</exception>
-    /// <exception cref="ArgumentException">Thrown when projectName or version is null or whitespace.</exception>
-    /// <returns>A new DeploymentNotification instance.</returns>
+    /// <param name="tests">The test instance.</param>
+    /// <param name="projectName">The project name.</param>
+    /// <param name="version">The version string.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="projectName"/> or <paramref name="version"/> is <see langword="null"/> or whitespace.</exception>
+    /// <returns>A new <see cref="DeploymentNotification"/> instance.</returns>
     public static DeploymentNotification CreateValidNotification(this NotificationTests tests, string projectName = "TestProject", string version = "1.0.0")
     {
         ArgumentNullException.ThrowIfNull(tests);
@@ -49,12 +49,17 @@ public static class NotificationTestsExtensions
     /// <summary>
     /// Creates a <see cref="ChannelConfiguration"/> instance for testing purposes.
     /// </summary>
-    /// <param name="tests">The test instance</param>
-    /// <param name="isEnabled">Whether the channel is enabled</param>
-    /// <param name="minPriority">The minimum priority for the channel</param>
-    /// <exception cref="ArgumentNullException">Thrown when tests is null.</exception>
-    /// <returns>A new ChannelConfiguration instance.</returns>
-    public static ChannelConfiguration CreateChannelConfig(this NotificationTests tests, bool isEnabled = true, NotificationPriority minPriority = NotificationPriority.Low)
+    /// <param name="tests">The test instance.</param>
+    /// <param name="isEnabled">Whether the channel is enabled.</param>
+    /// <param name="minPriority">The minimum priority for the channel.</param>
+    /// <param name="channelType">The notification channel type.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/>.</exception>
+    /// <returns>A new <see cref="ChannelConfiguration"/> instance.</returns>
+    public static ChannelConfiguration CreateChannelConfig(
+        this NotificationTests tests,
+        bool isEnabled = true,
+        NotificationPriority minPriority = NotificationPriority.Low,
+        NotificationChannel channelType = NotificationChannel.Slack)
     {
         ArgumentNullException.ThrowIfNull(tests);
 
@@ -62,7 +67,13 @@ public static class NotificationTestsExtensions
         {
             IsEnabled = isEnabled,
             MinimumPriority = minPriority,
-            DisplayName = "TestChannel"
+            DisplayName = "TestChannel",
+            ChannelType = channelType,
+            WebhookUrl = "https://example.com/webhook",
+            TargetId = "test-channel-id",
+            ApiToken = "test-token",
+            TimeoutMs = 5000,
+            MaxRetries = 2
         };
     }
 }
