a4d7318aa502353d2a90a4df84d01dc99d933826
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    cleanup.

--- tests/dotnet-deploy-notify.Tests/RollbackNotificationServiceTestsJsonExtensions.cs
+++ tests/dotnet-deploy-notify.Tests/RollbackNotificationServiceTestsJsonExtensions.cs
@@ -2,6 +2,9 @@ using System.Text.Json;
 
 namespace DotNetDeployNotify.Tests;
 
+/// <summary>
+/// Provides JSON serialization and deserialization extension methods for <see cref="RollbackNotificationServiceTests"/> objects.
+/// </summary>
 public static class RollbackNotificationServiceTestsJsonExtensions
 {
     private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
@@ -10,6 +13,21 @@ public static class RollbackNotificationServiceTestsJsonExtensions
         WriteIndented = false
     };
 
+    /// <summary>
+    /// Serializes a <see cref="RollbackNotificationServiceTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The <see cref="RollbackNotificationServiceTests"/> instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for better readability.</param>
+    /// <returns>A JSON string representation of the <see cref="RollbackNotificationServiceTests"/> instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+
+    /// <summary>
+    /// Serializes a <see cref="RollbackNotificationServiceTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The <see cref="RollbackNotificationServiceTests"/> instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for better readability.</param>
+    /// <returns>A JSON string representation of the <see cref="RollbackNotificationServiceTests"/> instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
     public static string ToJson(this RollbackNotificationServiceTests value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -20,6 +38,12 @@ public static class RollbackNotificationServiceTestsJsonExtensions
         return JsonSerializer.Serialize(value, _jsonSerializerOptions);
     }
 
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="RollbackNotificationServiceTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A <see cref="RollbackNotificationServiceTests"/> instance if deserialization succeeds; otherwise, null.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
     public static RollbackNotificationServiceTests? FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
@@ -33,6 +57,13 @@ public static class RollbackNotificationServiceTestsJsonExtensions
         }
     }
 
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="RollbackNotificationServiceTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">When this method returns, contains the deserialized <see cref="RollbackNotificationServiceTests"/> instance if successful; otherwise, null.</param>
+    /// <returns>true if deserialization succeeds; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
     public static bool TryFromJson(string json, out RollbackNotificationServiceTests? value)
     {
         ArgumentNullException.ThrowIfNull(json);
