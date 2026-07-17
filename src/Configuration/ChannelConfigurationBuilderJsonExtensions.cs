59c04b6b02042ecd8545e96933bc7db68a55476a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Polish ChannelConfigurationBuilderJsonExtensions

--- src/Configuration/ChannelConfigurationBuilderJsonExtensions.cs
+++ src/Configuration/ChannelConfigurationBuilderJsonExtensions.cs
@@ -2,7 +2,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =====================================================================
+// ===================================================================
 
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
@@ -36,32 +36,5 @@ public static class ChannelConfigurationBuilderJsonExtensions
         var config = value.Build();
 
         return JsonSerializer.Serialize(config, indented ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true } : _jsonOptions);
-    }
-
-    /// <summary>
-    /// Deserializes a JSON string to a <see cref="ChannelConfigurationBuilder"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized builder, or null if the JSON is null or empty.
-    /// Note: Due to internal implementation constraints of <see cref="ChannelConfigurationBuilder"/>,
-    /// deserialization from JSON is not supported. This method always returns null.</returns>
-    /// <exception cref="NotSupportedException">Always thrown to indicate deserialization is not supported.</exception>
-    public static ChannelConfigurationBuilder? FromJson(string json)
-    {
-        throw new NotSupportedException(
-            "Deserialization from JSON to ChannelConfigurationBuilder is not supported due to internal implementation constraints. " +
-            "Use ToJson() for serialization only.");
-    }
-
-    /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="ChannelConfigurationBuilder"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized builder if successful.</param>
-    /// <returns>Always false since deserialization is not supported.</returns>
-    public static bool TryFromJson(string json, out ChannelConfigurationBuilder? value)
-    {
-        value = null;
-        return false;
     }
 }
