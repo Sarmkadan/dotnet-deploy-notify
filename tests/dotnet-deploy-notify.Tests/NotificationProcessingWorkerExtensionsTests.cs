583c3fa5e70ae0dade4d3fde448ff23a5989c45c
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 21:30:00 +0300

    tweak NotificationProcessingWorkerExtensionsTests

---
+++ tests/dotnet-deploy-notify.Tests/NotificationProcessingWorkerExtensionsTests.cs
@@ -0,0 +1,14 @@
+// tests/dotnet-deploy-notify.Tests/NotificationProcessingWorkerExtensionsTests.cs
+
+using Xunit;
+using System.Threading.Tasks;
+using System;
+using DotNetDeployNotify.BackgroundWorkers;
+
+namespace DotNetDeployNotify.Tests.BackgroundWorkers
+{
+    public class NotificationProcessingWorkerExtensionsTests
+    {
+        // ... rest of the file ...
+    }
+}
