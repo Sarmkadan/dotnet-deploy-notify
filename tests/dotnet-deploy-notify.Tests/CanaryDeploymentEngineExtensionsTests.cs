c362ccda82925b5045657f13f892929bcc50991b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 09:52:15 +0300

    fix the build

---
+++ tests/dotnet-deploy-notify.Tests/CanaryDeploymentEngineExtensionsTests.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace dotnet_deploy_notify.Tests
+{
+    public class CanaryDeploymentEngineExtensionsTests
+    {
+        [Fact]
+        public async Task TestTryAdvanceRolloutAsync_ValidInput_ReturnsTrue()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestTryAdvanceRolloutAsync_InvalidInput_ThrowsException()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestTryPromoteAsync_ValidInput_ReturnsTrue()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestTryPromoteAsync_InvalidInput_ThrowsException()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestTryAbortAsync_ValidInput_ReturnsTrue()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestTryAbortAsync_InvalidInput_ThrowsException()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestGetCanaryPercentageNormalizedAsync_ValidInput_ReturnsDouble()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestGetCanaryPercentageNormalizedAsync_InvalidInput_ThrowsException()
+        {
+            // Test implementation here
+        }
+    }
+}
