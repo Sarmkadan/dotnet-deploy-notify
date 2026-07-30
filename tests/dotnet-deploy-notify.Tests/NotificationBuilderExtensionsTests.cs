3e3b7725453b2592a20e329873c7f0e46bf95662
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 20:08:14 +0300

    chore(dotnet-deploy-notify.tests): tweak NotificationBuilderExtensionsTe

---
+++ tests/dotnet-deploy-notify.Tests/NotificationBuilderExtensionsTests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace dotnet_deploy_notify.Tests
+{
+    public class NotificationBuilderExtensionsTests
+    {
+        // TODO: Write tests for each major public method
+    }
+}
