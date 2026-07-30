3a09a86dfc43603d86af07694e9af6ff47c5a774
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 13:36:08 +0300

    CommandParserExtensionsTests.cs

---
+++ tests/dotnet-deploy-notify.Tests/CommandParserExtensionsTests.cs
@@ -0,0 +1,44 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+
+namespace dotnet_deploy_notify.Tests
+{
+    public class CommandParserExtensionsTests
+    {
+        [Fact]
+        public async Task Test_Parse_Command_With_No_Arguments()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task Test_Parse_Command_With_Null_Input()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task Test_Parse_Command_With_Empty_Collection()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task Test_Parse_Command_With_Boundary_Value()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task Test_Parse_Command_With_Expected_Exception()
+        {
+            // Test implementation here
+        }
+    }
+}
