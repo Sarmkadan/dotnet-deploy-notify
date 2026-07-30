cd06b36657765cc2485b088c442cca95c9ff59c2
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 16:14:32 +0300

    tweak DryRunRenderResultTests

---
+++ tests/dotnet-deploy-notify.Tests/DryRunRenderResultTests.cs
@@ -0,0 +1,17 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetDeployNotify.Core;
+using DotNetDeployNotify.Core.Models;
+using DotNetDeployNotify.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace DotNetDeployNotify.Tests;
+
+public class DryRunRenderResultTests
+{
+    // ... entire content of the file ...
+}
