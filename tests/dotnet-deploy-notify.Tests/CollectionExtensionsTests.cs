c1710f562a24f59fb87cf55056ae77e151d02a59
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-31 18:17:40 +0300

    Update CollectionExtensionsTests

---
+++ tests/dotnet-deploy-notify.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace dotnet_deploy_notify.Tests
+{
+    public class CollectionExtensionsTests
+    {
+        // Test methods go here
+    }
+}
